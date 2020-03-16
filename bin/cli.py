b7632b55e06fe13dd5f5ae7599df020d8f1a10c7
sumanthratna <sumanthratna@gmail.com>
2020-03-16 17:58:29 -0400

    Add support for npy files

--- bin/cli.py
+++ bin/cli.py
@@ -6,6 +6,7 @@ import xml.etree.ElementTree as ET
 from PIL import Image
 import numpy as np
 from viewmask.utils import xml_to_contours, centers_of_contours, xml_to_image
+from os.path import splitext
 
 
 @click.group()
@@ -17,10 +18,17 @@ def cli():
 @click.argument('annotations', type=click.Path(exists=True, dir_okay=False))
 @click.option('-i', '--interactive', default=False, show_default=True, type=bool, is_flag=True)
 def view_annotations(annotations, interactive):
-    tree = ET.parse(annotations)
+    if splitext(annotations)[1] == '.npy' and interactive is True:
+        raise ValueError(
+            "The interactive flag cannot be passed with a numpy mask.")
+
+    # TODO: get rid of this, some XMLs might actually be unparseable:
+    try:
+        tree = ET.parse(annotations)
+    except ET.ParseError:
+        pass
     with napari.gui_qt():
         viewer = napari.Viewer()
-        tree = ET.parse(annotations)
         if interactive:
             regions = xml_to_contours(tree, 'napari')
             decimal_color = tree.find('./Annotation').attrib['LineColor']
@@ -29,7 +37,10 @@ def view_annotations(annotations, interactive):
                 regions, shape_type='path', edge_color=f"#{line_color}")
             viewer.add_points(centers_of_contours(regions))
         else:
-            rendered_annotations = xml_to_image(tree)
+            if splitext(annotations)[1] == '.npy':
+                rendered_annotations = np.load(annotations)
+            else:
+                rendered_annotations = xml_to_image(tree)
             viewer.add_image(
                 rendered_annotations,
                 name='annotations',
@@ -41,7 +52,11 @@ def view_annotations(annotations, interactive):
 @click.argument('image', type=click.Path(exists=True, dir_okay=False))
 def view_image(image):
     with napari.gui_qt():
-        _ = napari.view_image(np.asarray(Image.open(image)), name='image')
+        if splitext(image)[1] == '.npy':
+            np_img = np.load(image)
+        else:
+            np_img = np.array(Image.open(image))
+        _ = napari.view_image(np_img, name='image')
 
 
 @cli.command()
@@ -49,30 +64,35 @@ def view_image(image):
 @click.argument('annotations', type=click.Path(exists=True, dir_okay=False))
 @click.option('-i', '--interactive', default=False, show_default=True, type=bool, is_flag=True)
 def view_overlay(image, annotations, interactive):
+    if splitext(annotations)[1] == '.npy' and interactive is True:
+        raise ValueError(
+            "The interactive flag cannot be passed with a numpy mask.")
+
+    # TODO: get rid of this, some XMLs might actually be unparseable:
+    try:
+        tree = ET.parse(annotations)
+    except ET.ParseError:
+        pass
     with napari.gui_qt():
         viewer = napari.Viewer()
-        np_img = np.array(Image.open(image))
+        if splitext(image)[1] == '.npy':
+            np_img = np.load(image)
+        else:
+            np_img = np.array(Image.open(image))
         viewer.add_image(np_img, name='image')
 
-        tree = ET.parse(annotations)
         if interactive:
-            root = tree.getroot()
-            regions = []
-            for region in root.iter("Vertices"):
-                coords = []
-                for vertex in region:
-                    # convert each coordinate to (y, x) to fix the orientation
-                    coords.append(
-                        [float(vertex.get("Y")), float(vertex.get("X"))])
-                coords = np.array(coords)
-                regions.append(coords)
+            regions = xml_to_contours(tree, 'napari')
             decimal_color = tree.find('./Annotation').attrib['LineColor']
             line_color = hex(int(decimal_color)).replace('0x', '').zfill(6)
             viewer.add_shapes(
                 regions, shape_type='path', edge_color=f"#{line_color}")
-            viewer.add_points(centers_of_contours(regions), size=4)
+            viewer.add_points(centers_of_contours(regions))
         else:
-            rendered_annotations = xml_to_image(tree, shape=np_img.shape)
+            if splitext(annotations)[1] == '.npy':
+                rendered_annotations = np.load(annotations)
+            else:
+                rendered_annotations = xml_to_image(tree, shape=np_img.shape)
             viewer.add_image(
                 rendered_annotations,
                 name='annotations',
