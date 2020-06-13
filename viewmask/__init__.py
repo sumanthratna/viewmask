324bb8d2f3e1e4fec46bc7bf5fcc426893c1026e
Sumanth Ratna <sumanthratna@gmail.com>
2020-06-13 18:02:55 -0400

    Start object-oriented API These classes: - are not currently in use - use Python `UserList`s instead of `numpy.ndarray`s - don't use numpy's dispatch mechanism - don't have unit tests

--- viewmask/__init__.py
+++ viewmask/__init__.py
@@ -1,6 +1,74 @@
+from collections import UserList
 __version__ = '0.1.15'
 
 from viewmask import utils  # noqa  # pylint: disable=unused-import
+
+
+class AnnotationCoordinate:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    @property
+    def x(self):
+        return self._x
+
+    @x.setter
+    def x(self, value):
+        self._x = value
+
+    @property
+    def y(self):
+        return self._y
+
+    @y.setter
+    def y(self, value):
+        self._y = value
+
+    def __str__(self):
+        return f'({self.x}, {self.y})'
+
+    def __eq__(self, other):
+        return self.y == other.y and self.x == other.x
+
+    def __repr__(self):
+        return f"Coordinate ({self.x}, {self.y})"
+
+
+class MaskContour(UserList):
+    pass
+
+
+class Annotations(UserList):
+    def __init__(self, initlist=()):
+        UserList.__init__(self)
+        if isinstance(initlist, Annotations):
+            # Already deduped, copy without duplicate checking
+            self.data[:] = Annotations.data
+        else:
+            self.extend(initlist)
+
+    def check(self, v):
+        if not isinstance(v, MaskContour):
+            raise TypeError(f'item is not of type {MaskContour}')
+
+    def __add__(self, other):
+        ret = self.__class__(self)  # Alt: self.copy()
+        ret.extend(other)
+        return ret
+
+    def __setitem__(self, i, v):
+        self.check(v)
+        self.list[i] = v
+
+    def insert(self, i, v):
+        self.check(v)
+        super().insert(i, v)
+
+    def extend(self, other):
+        for x in other:
+            self.append(x)
+
 
 __all__ = [
     'utils.file_to_dask_array',
@@ -9,5 +77,7 @@ __all__ = [
     'utils.xml_to_image',
     'utils.get_stroke_color',
     'utils.mask_to_contours',
-    'utils.centers_to_image'
+    'utils.centers_to_image',
+    'AnnotationCoordinate',
+    'Annotations'
 ]
