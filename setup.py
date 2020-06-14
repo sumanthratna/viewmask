283be25fd866fee0fda934907f4732ba9d21f0a2
Sumanth Ratna <sumanthratna@gmail.com>
2020-06-14 16:31:42 -0400

    [collaborate] Add django.db.models.ImageField to AnnotatedImage model
    Co-Authored-By: null <KaienY@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ setup(
         'toolz==0.*,>=0.10.0'
     ],
     extras_require={"collaborate": [
-        "django >= 3.0.7",
-        "django-tailwind >= 0.9.0",
+        "django>=3.0.7",
+        "django-tailwind>=0.9.0",
     ]},
 )
--- viewmask/collaborate/collaborate/models.py
+++ viewmask/collaborate/collaborate/models.py
@@ -6,5 +6,10 @@ class AnnotatedImage(models.Model):
     # SHA hash of (image object and current time):
     unique_id = models.CharField(max_length=255)
 
+    # @sumanthratna: I haven't yet tested if Pillow works with SVS images
+    # TODO: if it doesn't work, just switch to FileField
+    # https://github.com/django/django/blob/a8473b4d348776d823b7a83c1795279279cf3ab5/django/forms/fields.py#L609-L665
+    image = models.ImageField(upload_to='uploaded_images/')
+
     # https://docs.djangoproject.com/en/3.0/topics/db/examples/many_to_many/
     users = models.ManyToManyField(User)
