c2cd52c75cba0bb5221b37cb03aa651cc1852a91
Sumanth Ratna <sumanthratna@gmail.com>
2020-06-13 21:24:56 -0400

    [collaborate] Add AnnotatedImage Django model

--- viewmask/collaborate/collaborate/models.py
+++ viewmask/collaborate/collaborate/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class AnnotatedImage(models.Model):
+    # SHA hash of (image object and current time):
+    unique_id = models.CharField(max_length=255)
+
+    # https://docs.djangoproject.com/en/3.0/topics/db/examples/many_to_many/
+    users = models.ManyToManyField(User)
