48eb232162c0a4b11fd894b92f60ec7512e4e50e
Sumanth Ratna <sumanthratna@gmail.com>
2020-06-13 20:00:12 -0400

    [collaborate] Add routes

--- viewmask/collaborate/collaborate/views.py
+++ viewmask/collaborate/collaborate/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def index(request):
+    if request.user.is_authenticated:
+        # Do something for authenticated users.
+        pass
+    else:
+        # Do something for anonymous users.
+        pass
+    return HttpResponse('auth' if request.user.is_authenticated else 'no auth')
+
+
+def annotate(request):
+    return HttpResponse('annotate')
--- viewmask/collaborate/urls.py
+++ viewmask/collaborate/urls.py
@@ -14,8 +14,22 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from collaborate import views
+
+# routes under include('django.contrib.auth.urls'):
+# accounts/login/ [name='login']
+# accounts/logout/ [name='logout']
+# accounts/password_change/ [name='password_change']
+# accounts/password_change/done/ [name='password_change_done']
+# accounts/password_reset/ [name='password_reset']
+# accounts/password_reset/done/ [name='password_reset_done']
+# accounts/reset/<uidb64>/<token>/ [name='password_reset_confirm']
+# accounts/reset/done/ [name='password_reset_complete']
 
 urlpatterns = [
+    path('', views.index),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('annotate', views.annotate),
     path('admin/', admin.site.urls),
 ]
